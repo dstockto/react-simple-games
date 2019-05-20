47762caa6ed02b64d41afaa21ae134e32ec9c188
Karen Garcia <karen.garcia@nexscreen.com>
2019-05-20 16:16:30 -0600

    Dave helped adjustment to class

--- src/Components/Games/EliMath/EliMathGame.jsx
+++ src/Components/Games/EliMath/EliMathGame.jsx
@@ -1,7 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+
+
+
+// function myAlert(idName){
+//   const result = document.getElementById("idName").innerHTML;
+//   alert(result);
+// }
+
+class numberGenerator {
+  const (firstNum, operator, secNum, userAnswer){
+    this.firstNum = firstNum;
+    this.operator = operator;
+    this.secNum = secNum;
+    this.userAnswer = userAnswer;
+  }
+
+  add() {
+
+    return this.firstNum + this.secondNum;
+  }
+
+}
+
+// const firstNum = document.getElementById("firstNum").innerHTML;
+// const secNum = document.getElementById("secNum").innerHTML;
+
+// // let karenAdd = new numberGenerator(firstNum,secNum);
+// // karenAdd.add();
+
+// function addMath () {
+//   const firstNum = parseInt(document.getElementById("firstNum").innerHTML);
+//   const secNum = parseInt(document.getElementById("secNum").innerHTML);
+//   var result = firstNum + secNum;
+//   return result;
+// }
 
 export default function EliMathGame() {
+  const myProblem = new numberGenerator(1, "+", 2, 4);
+  
+  //put state here
+
   return (
-    <div>Eli's Math Game coming soon!</div>
+    <div 
+      className="mathGame" 
+      id="eliTitle"
+    >
+      Eli's Math Game
+    <div
+      id="eliMathContent"
+    >
+        <div
+        id="equationsEli"
+        >
+          Equations
+          <h1> {myProblem.firstNum} {myProblem.operator} {myProblem.secNum} =</h1>
+      
+          <input 
+            type={"text"}
+            pattern="[0-9]*" 
+            placeholder = "Answer"
+            value={myProblem.userAnswer}
+            onKeyDown={addMath}
+            
+          />
+          
+          
+        </div>
+        <div
+        id="scoringEli"
+        >
+          Scoring
+        </div>
+        <div
+        id="else"
+        >
+          Something else
+        </div>
+      </div> 
+   </div>
+    
   );
 };
