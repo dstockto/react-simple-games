57831c608a82860bd9917940fa7fd2752ae276ba
David Stockton <dstockton@i3logix.com>
2019-08-05 17:59:58 -0600

    Publishing

---
+++ precache-manifest.ac9b9f6a8a015f8008bb88efae0f0fb3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7883baf6eb80d6be1523fc948c4c0509",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5e6cb1934d51a82ba3b",
+    "url": "/static/css/main.59dfd960.chunk.css"
+  },
+  {
+    "revision": "377ecda9be3a88eabe96",
+    "url": "/static/js/2.5c97c44a.chunk.js"
+  },
+  {
+    "revision": "b5e6cb1934d51a82ba3b",
+    "url": "/static/js/main.f7a32380.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0d01e6962fde9915df5af64cfb88f535",
+    "url": "/static/media/nonogram.0d01e696.png"
+  },
+  {
+    "revision": "aba201d7da36b0667efe6f9ae8a5f080",
+    "url": "/static/media/sudoku.aba201d7.png"
+  }
+]);
