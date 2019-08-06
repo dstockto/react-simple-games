cfc573411e6ba00d14df891d8974d7dd4e063843
David Stockton <dstockton@i3logix.com>
2019-08-05 18:03:52 -0600

    More publishing

---
+++ precache-manifest.54ea153be8cc00b27a5cb0d81ebc5e60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69ef50adc0d4891d8e71fa0029f7cb48",
+    "url": "/react-simple-games/index.html"
+  },
+  {
+    "revision": "eebc50cc32aea6bfce8c",
+    "url": "/react-simple-games/static/css/main.59dfd960.chunk.css"
+  },
+  {
+    "revision": "377ecda9be3a88eabe96",
+    "url": "/react-simple-games/static/js/2.5c97c44a.chunk.js"
+  },
+  {
+    "revision": "eebc50cc32aea6bfce8c",
+    "url": "/react-simple-games/static/js/main.a3dd32c3.chunk.js"
+  },
+  {
+    "revision": "3f84ab3ef307673aa32f",
+    "url": "/react-simple-games/static/js/runtime~main.ad71673e.js"
+  },
+  {
+    "revision": "0d01e6962fde9915df5af64cfb88f535",
+    "url": "/react-simple-games/static/media/nonogram.0d01e696.png"
+  },
+  {
+    "revision": "aba201d7da36b0667efe6f9ae8a5f080",
+    "url": "/react-simple-games/static/media/sudoku.aba201d7.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ac9b9f6a8a015f8008bb88efae0f0fb3.js"
+  "/react-simple-games/precache-manifest.54ea153be8cc00b27a5cb0d81ebc5e60.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-simple-games/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
